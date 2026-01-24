a219744417b4bb0754c7e03e7eb7580a6e7d61aa
Koushik Chatterjee <koushik350@gmail.com>
2026-01-24 18:02:48 +0530

    🚀 RELEASE: 4.0.5

--- playwright.config.ts
+++ playwright.config.ts
@@ -52,10 +52,10 @@ export default defineConfig({
     // },
 
     /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
+    {
+      name: "Mobile Chrome",
+      use: { ...devices["Pixel 5"] },
+    },
     // {
     //   name: 'Mobile Safari',
     //   use: { ...devices['iPhone 12'] },
@@ -66,9 +66,9 @@ export default defineConfig({
     //   name: 'Microsoft Edge',
     //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
     // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
+    {
+      name: "Google Chrome",
+      use: { ...devices["Desktop Chrome"], channel: "chrome" },
+    },
   ],
 });
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -5,6 +5,6 @@ import { credentials } from "../utils/test-data";
 test("User can log in successfully @login @smoke", async ({ page }) => {
   const loginPage = new LoginPage(page);
   await loginPage.goto();
-  await loginPage.login(credentials.username, "credentials.password");
+  await loginPage.login(credentials.username, credentials.password);
   await expect(page).toHaveURL(/\/home/);
 });
